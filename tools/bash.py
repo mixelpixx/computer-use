b1cdb4f0a002c367422351fd7a09c6c4cb1a9b4b
mixelpixx <chris_stockslager@comcast.net>
2024-11-23 09:29:53 -0500

    Delete tools directory

--- tools/bash.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from typing import ClassVar
-
-class BashTool:
-    def execute(self, command: str):
-        # Execute Windows commands directly
-        if not command.startswith('cmd'):
-            command = f'cmd /c {command}'
-        subprocess.run(command, shell=True)
